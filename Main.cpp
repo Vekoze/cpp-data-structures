0b57a7da1dffa93e6bce28ba3bb187c505a5c189
vekxze <vekxze@proton.me>
2023-01-14 18:14:51 -0500

    linkedlist push/pop tests

---
+++ Main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include "LinkedList.h"
+
+int main() {
+
+	// LinkedList
+	LinkedList<int> ll;
+	ll.PushBack(3);
+	ll.PushBack(4);
+	ll.PushBack(5);
+	ll.PushFront(2);
+	ll.PushFront(1);
+	std::cout << ll << '\n';
+	std::cout << "pop: " << ll.Pop() << '\n';
+	std::cout << ll << '\n';
+	return 0;
+}
